920c8b93154a3c264fe24a39ef96d31189832724
Johny-Robit <jean.robitaille.2@ulaval.ca>
2025-09-20 14:34:00 -0400

    First commit manipulateur de chaines

--- 3A/ManipulateurDeChainesTest.cpp
+++ 3A/ManipulateurDeChainesTest.cpp
@@ -9,6 +9,8 @@ class ManipulateurDeChainesTest : public ::testing::Test {
     protected:
         // Vous pouvez ajouter des constantes ici
         const std::string CHAINE_VIDE = "";
+        const std::string CHAINE_NON_VIDE = "Muppets";
+        const std::string CHAINE_AVEC_ESPACES = "  Hello World !!! ";
 };
 
 TEST_F(ManipulateurDeChainesTest, EtantDonneChaineVide_QuandCompterCaracteres_AlorsLeResultatEstZero) {
@@ -25,19 +27,25 @@ TEST_F(ManipulateurDeChainesTest, EtantDonneChaineVide_QuandCompterCaracteres_Al
 // TODO: Écrire le test suivant avec le principe Préparation-Action-Validation
 TEST_F(ManipulateurDeChainesTest, EtantDonneChaineNonVide_QuandCompterCaracteres_AlorsLeResultatEstLeBonNombreDeCaracteres) {
     // Préparation
+    std::string chaine = CHAINE_NON_VIDE;
 
     // Action
+    int nbCaracteres = ManipulateurDeChaines::compterCaracteres(CHAINE_NON_VIDE);
 
     // Validation
+    EXPECT_GT(nbCaracteres, 0) << "La chaîne ne devrait pas être vide";
 }
 
 // TODO: Écrire le test suivant avec le principe Préparation-Action-Validation
 TEST_F(ManipulateurDeChainesTest, EtantDonneChaineAvecEspaces_QuandSupprimerEspaces_AlorsLesEspacesSontSupprimes) {
     // Préparation
+    std::string chaine = CHAINE_AVEC_ESPACES;
 
     // Action
+    std::string chaineSansEspaces = ManipulateurDeChaines::enleverEspaces(chaine);
 
     // Validation
+    EXPECT_FALSE(chaineSansEspaces.find(' ')) << "La chaîne ne devrait pas contenir d'espaces";
 }
 
 // TODO: Écrire le test suivant avec le principe Préparation-Action-Validation
